1809494e7750322becf226100360adcd15dab6ce
Unknown <keith@maibach.ca>
2018-08-29 21:12:44 -0700

    Add the decoding, buffering, parsing, and recognition of payload bodies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Dependencies
 const http = require('http');
 const url = require('url');
+const {StringDecoder} = require('string_decoder');
 
 //Instantiate the HTTP server
 let server = http.createServer((req, res) => {
@@ -9,23 +10,43 @@ let server = http.createServer((req, res) => {
     path = parsedUrl.path;
     let trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-    //Select the handler form the router
-    console.log(path);
-    chosenHandler = typeof(router[trimmedPath]) == 'undefined' ? router.notFound : router[trimmedPath];
-    
-    //Route the request to the chosen handler with the data
-    chosenHandler('Say it', (statusCode, payload) => {
-        //Return the response
-        res.setHeader('Content-Type', 'text/plain');
-
-        let payloadString = JSON.stringify(payload);
-        res.writeHead(statusCode);
-        res.end(payloadString);
+    let method = req.method.toLowerCase();
+    console.log(method)
+;    let decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
     });
+
+    req.on('end', () => {
+        console.log('On End');
+        let payload = buffer + decoder.end();
+
+        //Select the handler form the router
+        chosenHandler = typeof(router[trimmedPath]) == 'undefined' ? router.notFound : router[trimmedPath];
+        
+        var data = {
+            payload,
+            method
+        }
+
+        //Route the request to the chosen handler with the data
+        chosenHandler(data, (statusCode, payload) => {
+            //Return the response
+            res.setHeader('Content-Type', 'text/plain');
+
+            let payloadString = JSON.stringify(payload);
+            res.writeHead(statusCode);
+            res.end(payloadString);
+        });
+    });
+
+
 });
 
 //Start the HTTP server
 server.listen(8080);
+console.log('Server listening on port 8080');
 
 handlers = {};
 
@@ -34,7 +55,10 @@ handlers.notFound = (data, callback) => {
 }
 
 handlers.hello = (data, callback) => {
-    payload = data == 'Say it' ? 
+    console.log(data.payload);
+    payloadObject = JSON.parse(data.payload);
+    
+    payload = payloadObject.request == 'Say it' && data.method == 'post' ? 
     {'message' : 'Smoke weed every day' } :
     {'message' : 'Mind your manners'};
     statusCode = 200;
